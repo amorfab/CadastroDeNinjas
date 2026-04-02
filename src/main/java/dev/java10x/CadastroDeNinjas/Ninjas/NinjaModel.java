1a1b406d1ecc2fed07e137d81946184dbec6a210
Fabiana <fabiana_de_amorim@hotmail.com>
2026-04-02 16:54:53 -0300

    fix: Coloca a coluna de email do ninja como unico

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,6 +18,7 @@ public class NinjaModel {
     private Long id;
     private String nome;
     private int idade;
+    @Column(unique = true)
     private String email;
     // @ManyToOne - um ninja tem uma unica missao
     @ManyToOne
